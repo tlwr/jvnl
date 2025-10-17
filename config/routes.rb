b7d95b3ca3baddd1414361010641200ae0ff3a5b
toby lorne <toby@toby.codes>
2025-10-17 14:32:27 +0200

    basic user mgmt with devise
    Signed-off-by: toby lorne <toby@toby.codes>

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,16 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  devise_for :users, controllers: {
+    confirmations: 'users/confirmations',
+    passwords: 'users/passwords',
+    sessions: 'users/sessions',
+    unlocks: 'users/unlocks',
+  }
+
+  get "/inloggen", to: redirect('/users/sign_in')
+
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
