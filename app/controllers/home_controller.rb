fe549ab2bcad8f5876508dd09a6af3d73acc941c
toby lorne <toby@toby.codes>
2025-12-16 15:14:09 +0100

    add random route
    Signed-off-by: toby lorne <toby@toby.codes>

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,4 +6,13 @@ class HomeController < ApplicationController
       @pictures = Picture.where(visible: true).limit(3)
     end
   end
+
+  def random
+    picture = Picture.where(visible: true).order("RANDOM()").first
+    if picture
+      redirect_to picture_path(picture)
+    else
+      redirect_to root_path, notice: "Geen beelden beschikbaar"
+    end
+  end
 end
